0b207d4bb1034ad41beac698935bd9dc5f07a7c6
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-05-14 20:30:08 +0530

    Content Update

--- Hello/HelloServlet/src/Servlet/HelloServlet.java
+++
@@ -1,8 +0,0 @@
-package Servlet;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet(urlPatterns = "/abc")
-public class HelloServlet extends HttpServlet {
-}
