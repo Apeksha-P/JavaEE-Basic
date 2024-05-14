b506cd0aefa6b88b3f4ec120be3a410bb4549f40
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-05-14 14:31:46 +0530

    Create JavaEE project

---
+++ Hello/HelloServlet/src/Servlet/HelloServlet.java
@@ -0,0 +1,8 @@
+package Servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/abc")
+public class HelloServlet extends HttpServlet {
+}
